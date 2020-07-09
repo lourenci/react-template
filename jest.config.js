2018da477e529786b5c06e529add3b97670307df
Leandro Lourenci <leandro@lourenci.com>
2020-07-09 18:17:44 -0300

    Upgrade template

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom-sixteen',
   collectCoverage: true,
   collectCoverageFrom: ['src/**'],
   coverageDirectory: 'coverage',
   testMatch: ['<rootDir>/src/**/*.spec.js'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testRunner: 'jest-circus/runner',
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-    ]
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: './assets/index.html' })]
+  plugins: [new HtmlWebpackPlugin({ template: './assets/index.html' })],
 }
